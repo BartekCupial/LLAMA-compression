2e788c68bf72c1cd12cc830a07f0429dc9ac91be
Bartłomiej Cupiał <bartomiej.cupial@student.uj.edu.pl>
2023-05-21 11:46:38 +0200

    fix bug and parametrize arithmetic

--- example_arithmetic.py
+++ example_arithmetic.py
@@ -36,20 +36,20 @@ def decompress(freqs, bitin, tokenizer, out):
         decoded_tokens.append(symbol)
         freqs.increment(symbol)
 
-    decoded_data = tokenizer.decode(decoded_tokens.tolist()[0])
+    decoded_data = tokenizer.decode(decoded_tokens)
     out.write(decoded_data)
     print(decoded_data)
     return decoded_data
 
 
-def main(tokenizer_path: str):
+def main(tokenizer_path: str, enc_dir: str = "arithmetic_enc", dec_dir: str = "arithmetic_dec"):
     mem_before = psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)
     wiki = load_dataset("wikipedia", "20220301.en", split="train")
     mem_after = psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)
     print(f"RAM memory used: {(mem_after - mem_before)} MB")
 
-    enc_dir = Path("arithmetic_enc")
-    dec_dir = Path("arithmetic_dec")
+    enc_dir = Path(enc_dir)
+    dec_dir = Path(dec_dir)
     enc_dir.mkdir(parents=True, exist_ok=True)
     dec_dir.mkdir(parents=True, exist_ok=True)
 
